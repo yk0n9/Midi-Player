30f8dace5862cee4e3b2574f70d6b6724fb5132c
ykong <ykonghao@icloud.com>
2022-07-24 12:27:17 +0800

    optimization: decoupling

--- src/main/java/com/genshin/Playback.java
+++ src/main/java/com/genshin/Playback.java
@@ -16,12 +16,12 @@ public class Playback {
     private static Robot robot;
     private static final Map<Integer, Integer> key = KeyMapper.init();
 
-    public static void play(String path, double speed) throws Exception {
+    public static List<Map<String, Object>> init(File file) throws Exception {
 
-        Sequence sequence = MidiSystem.getSequence(new File(path));
+        Sequence sequence = MidiSystem.getSequence(file);
         int resolution = sequence.getResolution();
         Track[] tracks = sequence.getTracks();
-        List<MidiEvent> meta_message = new ArrayList<>();
+        List<MidiEvent> event_message = new ArrayList<>();
         List<Map<String, Object>> message = new ArrayList<>();
         Map<String, Object> map;
         long tempo = 500000;
@@ -32,15 +32,15 @@ public class Playback {
         //消息按播放顺序返回  就好像它们都在一条轨道上一样。
         for (Track t : tracks) {
             for (int i = 0, len = t.size(); i < len; i++) {
-                meta_message.add(t.get(i));
+                event_message.add(t.get(i));
             }
         }
-        meta_message.sort(Comparator.comparing(MidiEvent::getTick));
+        event_message.sort(Comparator.comparing(MidiEvent::getTick));
 
         //处理midi序列  根据每个序列区间不同的tempo赋予不同的时间差
         //Tick to Millisecond
         //milliseconds = tick difference * (tempo / 1000 / resolution)
-        for (MidiEvent midiEvent : meta_message) {
+        for (MidiEvent midiEvent : event_message) {
 
             double time;
             map = JSON.parseObject(JSON.toJSONString(midiEvent.getMessage()));
@@ -57,9 +57,11 @@ public class Playback {
                 map.put("time", time);
                 message.add(map);
             }
-
         }
+        return message;
+    }
 
+    public static void play(List<Map<String, Object>> message, double speed) throws InterruptedException {
         System.out.println("Start playing!");
 
         //播放所有序列  消息以正确的计时生成时间
@@ -131,7 +133,7 @@ public class Playback {
             }
 
             try {
-                Playback.play(file.getAbsolutePath(), speed);
+                play(init(file), speed);
             } catch (Exception e) {
                 e.printStackTrace();
             } finally {
